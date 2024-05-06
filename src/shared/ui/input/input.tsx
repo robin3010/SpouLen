e408c8f5fc689207639015254bc021870eaf0fa0
robin3010 <robin3010@gmail.com>
2024-05-07 01:34:15 +0300

    rewrited calculatorForm, form handlers to work with store data, actions

--- src/shared/ui/input/input.tsx
+++ src/shared/ui/input/input.tsx
@@ -1,3 +1,4 @@
+import { observer } from 'mobx-react-lite'
 import { FC, ReactNode } from 'react'
 import {
   Col,
@@ -15,31 +16,27 @@ interface FormGroupPropsWithCol extends FormGroupProps, ColProps {
   col?: boolean
 }
 
-export const FormControl: FC<FormControlPropsWithLabel> = ({
-  label,
-  size = 'sm',
-  ...controlProps
-}) => (
-  <>
-    {label ? <Form.Label column={size}>{label}</Form.Label> : null}
-    <Form.Control
-      // placeholder=''
-      {...controlProps}
-      size={size}
-      autoComplete='off'
-    />
-  </>
+export const FormControl: FC<FormControlPropsWithLabel> = observer(
+  ({ label, size = 'sm', ...controlProps }) => (
+    <>
+      {label ? <Form.Label column={size}>{label}</Form.Label> : null}
+      <Form.Control
+        // placeholder=''
+        {...controlProps}
+        size={size}
+        autoComplete='off'
+      />
+    </>
+  ),
 )
 
-export const FormGroup: FC<FormGroupPropsWithCol> = ({
-  col = true,
-  children,
-  ...groupProps
-}) => (
-  <Form.Group
-    {...groupProps}
-    as={col ? Col : undefined}
-  >
-    {children}
-  </Form.Group>
+export const FormGroup: FC<FormGroupPropsWithCol> = observer(
+  ({ col = true, children, ...groupProps }) => (
+    <Form.Group
+      {...groupProps}
+      as={col ? Col : undefined}
+    >
+      {children}
+    </Form.Group>
+  ),
 )
